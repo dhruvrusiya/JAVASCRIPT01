f2b883e036c2c31548a927b8a642dedc62af7be4
dhruv rusiya <dhruvrusiya070@gmail.com>
2025-01-09 19:53:47 +0530

    mathrandom function

---
+++ math.js
@@ -0,0 +1,21 @@
+let num=Math.trunc(Math.random()*15+1) //0-15
+console.log(num);
+
+
+
+function fun1(){
+    document.querySelector('#abc').innerHTML=Math.trunc(Math.random()*15+1)*1000
+    document.querySelector('#abc1').innerHTML=Math.trunc(Math.random()*15+1)
+    document.querySelector('#abc2').innerHTML=Math.trunc(Math.random()*15+1)
+}
+
+function a1(){
+    document.querySelector('#a1').innerHTML=Math.trunc(Math.random()*50+1)
+}
+
+function a2(){
+    document.querySelector('#a2').innerHTML=Math.trunc(Math.random()*50+1)
+}
+function a3(){
+    document.querySelector('#a3').innerHTML=Math.trunc(Math.random()*50+1)
+}
