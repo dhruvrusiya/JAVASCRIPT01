093d3cfc9273dab551f2626782f9edeba4fa0823
dhruv rusiya <dhruvrusiya070@gmail.com>
2025-01-10 20:01:53 +0530

    task

--- task2.js
+++ task2.js
@@ -1,3 +1,17 @@
-fun(){
+ 
+let a=250
+let result=0
+
+ function fun1(){
+ let sum1=document.querySelector('#sum');
+ let z=parseInt(sum1.innerText)
+ let x=prompt("enter quantity")
+
+  result=x*a
+  
+  let oddvalue= result+newvalue
+
+ 
+ sum1.innerHTML=`the total price is ${result}rs`
     
 }
