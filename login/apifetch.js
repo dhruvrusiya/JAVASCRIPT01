23667fe8625f29ad084f170896cdd334e54d356c
dhruv rusiya <dhruvrusiya070@gmail.com>
2025-01-27 20:03:52 +0530

    api fetch

---
+++ login/apifetch.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+async function fetdata(){
+    let data = await fetch()
+    let res = await data.json(
+        console.log(res)
+        
+    )
+}
+
+fetch()
--- login/login_page_ls.js
+++
@@ -1,6 +0,0 @@
-const data1 =()  =>{
-  let a=document.querySelector('#email1').value;
-  let b=document.querySelector('#pass1').value;
-
-let c=localStorage.getItem()
-}
