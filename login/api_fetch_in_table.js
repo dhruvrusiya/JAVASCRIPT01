765f6b57b442ff8522903d399c97d2b4ac15cc4c
dhruv rusiya <dhruvrusiya070@gmail.com>
2025-01-31 19:52:11 +0530

    api fetch from server or website into table

---
+++ login/api_fetch_in_table.js
@@ -0,0 +1,16 @@
+async function retrive_data(){
+    let data = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let final_data = await data.json()
+    let tdata = final_data.map((e)=>`
+       <tr>
+        <td> ${e.id} </td>
+        <td> ${e.title} </td>
+         <td> ${e.body} </td>
+     </tr>
+    
+    `).join("")
+    document.querySelector('#displaydata').innerHTML=tdata
+
+}
+
+retrive_data()
